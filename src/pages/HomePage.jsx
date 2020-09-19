09c6a52e7ecbfb83e10edde1ef206b6cecc81bde
Felertooshia <67173720+Felertooshia@users.noreply.github.com>
2020-09-19 12:55:17 +0800

    Updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import ProjectCard from "../components/ProjectCard/ProjectCard";
 import { allProjects } from "../data";
 
 function HomePage() {
     return (
-        <div>
+        <div id="project-list">
             {allProjects.map((projectData, key) => {
-                return <div key={key}>{projectData.title}</div>;
+                return <ProjectCard key={key} projectData={projectData} />;
             })}
         </div>
     );
